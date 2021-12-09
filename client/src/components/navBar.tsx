89a8b7d4d513259275532716b68e31aeff49b267
tingjunman <tm2838@nyu.edu>
2021-12-08 21:42:31 -0600

    add navBar

---
+++ client/src/components/navBar.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import {
+  AppBar, Box, Toolbar, Typography, IconButton, Container, Avatar, Menu, MenuItem,
+} from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import UserProfile from '../static/userProfile.png';
+
+const settings = ['Wish list', 'Visited'];
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
+
+const NavBar: React.FC = () => {
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    console.log(event.currentTarget); //eslint-disable-line
+    setAnchorElUser(null);
+  };
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar position='static'>
+        <Container maxWidth='xl'>
+          <Toolbar disableGutters>
+            <Typography
+              variant='h6'
+              noWrap
+              component='div'
+              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            >
+              SPACE OBSERVER
+            </Typography>
+            <Box>
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt='user profile' src={UserProfile} />
+              </IconButton>
+              <Menu
+                sx={{ mt: '45px' }}
+                id='menu-appbar'
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign='center'>{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default NavBar;
